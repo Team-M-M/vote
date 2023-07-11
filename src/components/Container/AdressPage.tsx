0c8bfb610cb6b91c85778b5a3a601625cc67d629
Parkwochang <whwh3560@gmail.com>
2023-07-11 12:08:54 +0900

    asd

--- src/components/Container/AdressPage.tsx
+++ src/components/Container/AdressPage.tsx
@@ -6,10 +6,10 @@ export function AdressPage() {
   const router = useRouter();
 
   return (
-    <div className="h-screen flex justify-center items-center flex-col  px-10" style={{ background: `url(/images/group-300.png) no-repeat center 170px` }}>
-      <div className="h-[100px]"></div>
+    <div className="h-screen flex justify-center items-center flex-col  px-10" style={{ background: `url(/images/group-300.png) no-repeat center 160px` }}>
+      <div className="h-[90px]"></div>
       <h1 className="text-main text-center text-2xl font-bold">하남 미사 푸르지오시티 A동</h1>
-      <div className="h-[30px]"></div>
+      <div className="h-[20px]"></div>
       <div className="text-2xl">
         <h2 className="font-semibold text-gray-500">
           안녕하세요.{'\u00A0'}
