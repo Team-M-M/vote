3d848ea0d78fc883fd4cf5bbb75a148fba89480b
Parkwochang <whwh3560@gmail.com>
2023-07-12 15:50:03 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { headers } from "next/dist/client/components/headers";
+import { cookies } from 'next/headers'
+
+
 export default function Home() {
+  // const headersList = headers()
+  // headersList.get('')
+
+  const cookieStore = cookies()
+  const member = cookieStore.get('member')
+
+  console.log(member?.value)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>인트로 페이지</div>
+      <div>전자투표 시스템</div>
     </main>
   );
 }
