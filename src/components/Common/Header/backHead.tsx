1111d4b123ceca313a6ca78a4611aa18896401d2
Parkwochang <whwh3560@gmail.com>
2023-07-16 15:13:50 +0900

    change h

--- src/components/Common/Header/backHead.tsx
+++ src/components/Common/Header/backHead.tsx
@@ -1,29 +1,33 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { Spacing } from '../Spacing';
 
 export const BackHeader = () => {
   const router = useRouter();
 
   return (
-    <nav className="fixed w-full top-0 bg-white" style={{ zIndex: 999 }}>
-      <div className="flex justify-between items-center py-5 px-3 border-b-[1px] border-[#ECECEC]">
-        <div className="flex flex-auto items-center">
-          <button type="button" className="inline-flex items-center  text-sm text-black" onClick={() => router.back()}>
-            <svg
-              className="w-7 h-7"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2.5"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5"></path>
-            </svg>
-          </button>
+    <div>
+      <nav className="fixed h-16 w-full top-0 bg-white" style={{ zIndex: 999 }}>
+        <div className="flex justify-between items-center py-5 px-3 border-b-[1px] border-[#ECECEC]">
+          <div className="flex flex-auto items-center">
+            <button type="button" className="inline-flex items-center  text-sm text-black" onClick={() => router.back()}>
+              <svg
+                className="w-7 h-7"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5"></path>
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <Spacing size={64} />
+    </div>
   );
 };
