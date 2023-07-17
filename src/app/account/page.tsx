0cc6f30c9890fbd8d814052c0b4ce9026c0bb4dc
Parkwochang <whwh3560@gmail.com>
2023-07-17 13:12:03 +0900

    add

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,15 +1,13 @@
 import { BackHeader } from '@components/Common/Header/backHead';
 import { ToastContainer } from '@components/Common/Modal/Toast';
 import AccountPage from '@components/Container/AccountPage';
-import { cookies } from 'next/headers';
 
 const Page = () => {
-  const cookie = cookies()
 
   return (
     <main >
       <BackHeader />
-      <AccountPage cookie={cookie} />
+      <AccountPage />
       {/* <KeyPad /> */}
     </main>
   );
