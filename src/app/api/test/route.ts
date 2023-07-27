411867f4f689dac5c79ae2f4de579a7b93033f8f
signkj <signkj@nate.com>
2023-07-28 00:30:51 +0900

    test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,20 +1,19 @@
-
 // import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 
+//test
 // ! cookie로 로그인 data 전달을 위한 api입니다.
 export async function POST(req: Request) {
-  const body = await req.json()
+  const body = await req.json();
 
   if (req.method === 'POST') {
-    const { name, id, detail, dongho }: any = body
+    const { name, id, detail, dongho }: any = body;
     const response = NextResponse.json({ code: 201, message: 'success' });
 
     response.cookies.set({
       name: 'user',
       value: JSON.stringify({ name, id, detail, dongho }),
-    })
-
+    });
 
     return response;
   }
