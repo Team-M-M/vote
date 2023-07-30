cbdf2ea55a4eee6fdcd3a3e0bfbf325c038fbb82
signkj <signkj@nate.com>
2023-07-30 12:47:53 +0900

    title change

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -3,9 +3,12 @@ export const API_URL = {
   CLIENT: process.env.NEXT_PUBLIC_SERVER_URL,
   //! 리스트
   VOTE_LIST: '/office/vote_projectnew',
-  // ! 후보자 
+  // ! 후보자
   CANDIDATE: '/office/player',
-  //! 투표하기
+  //! 투표하기 - 관리인은 한명만 투표 가능
   VOTE_DO: '/office/vote',
-
+  //! 여러명 투표하기 - 관리위원은 여러명을 투표할 수 있다.
+  VOTE_MULTI_DO: '/office/votemulti',
+  //! 현재 선거별로 투표율 표기
+  VOTE_CUR_RATE: '/office/voteresult',
 };
