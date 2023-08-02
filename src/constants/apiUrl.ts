1a2814789f6cf5c9d1243d0e08eaa6cc99d3ac50
signkj <signkj@nate.com>
2023-08-02 11:43:36 +0900

    auth user

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,6 +1,8 @@
 export const API_URL = {
   BASE_URL: process.env.SERVER_URL,
   CLIENT: process.env.NEXT_PUBLIC_SERVER_URL,
+  //! 사용자 인증
+  USER_VOTE: '/office/user',
   //! 리스트
   VOTE_LIST: '/office/vote_projectnew',
   // ! 후보자
