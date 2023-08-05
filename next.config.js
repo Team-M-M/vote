e389bac9d5da4ddb043efcc89a588fd0904c5de6
signkj <signkj@nate.com>
2023-08-05 11:04:38 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: {
+    domains: ['localhost', '*'],
+  },
   reactStrictMode: false,
   // webpackDevMiddleware: null,
   swcMinify: true,
