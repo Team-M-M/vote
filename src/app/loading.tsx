dbe51be613e510932aebfaf621aa05a6bd065d77
Parkwochang <whwh3560@gmail.com>
2023-08-10 18:00:32 +0900

    loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,19 +1,7 @@
-'use client'
-
-import Lottie from "react-lottie-player";
-import lottieJson from "../../public/images/loading.json"
+import LoadingPage from "@components/Common/Loading/LoadingPage";
 
 export default function Loading() {
   return (
-    <div className="bg-white text-black px-5">
-      <div className="flex justify-center items-center">
-        <Lottie
-          loop
-          animationData={lottieJson}
-          play
-          style={{ width: 350, height: 350 }}
-        />
-      </div>
-    </div>
+    <LoadingPage />
   );
 }
---
+++ src/components/Common/Loading/LoadingPage.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Lottie from "react-lottie-player";
+import lottieJson from "../../../../public/images/loading.json"
+import { Spacing } from "../Spacing";
+
+export default function LoadingPage() {
+  return (
+    <div className="bg-white text-black px-5">
+      <Spacing size={120} />
+      <div className="flex justify-center items-center">
+        <Lottie
+          loop
+          animationData={lottieJson}
+          play
+          style={{ width: 350, height: 350 }}
+        />
+      </div>
+    </div>
+  );
+}
