d37b9946895b8266742d24f34f4d568f471ddf46
Parkwochang <whwh3560@gmail.com>
2023-08-24 00:43:30 +0900

    test: [front] commit-msg 수정

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -7,14 +7,17 @@ module.exports = {
     {
       rules: {
         'develop-rule': ({ subject }) => {
-          const commitFolders = ['[frontend]', '[backend]', '[domain]', '[root]'];
+          const commitFolders = ['[front]', '[back]', '[domain]', '[root]'];
           return [
             commitFolders.some(folder => subject?.startsWith(folder) !== subject?.endsWith(folder)),
             `\n${commitFolders.map(folder => `${folder}\n`).join('')}
 위 네 가지 중 한 가지는 반드시 콜론(:) 뒤에 포함되어야 합니다.
 [name] 뒤에 메시지 입력은 필수입니다.
 
-ex) feat: [frontend] 마우스 더블클릭 후 스타일 변경`,
+type = [build, ci, docs, feat, fix, perf, refactor, test, revert]
+
+ex) type: [name] message
+ex) feat: [front] 마우스 더블클릭 후 스타일 변경`,
           ];
         },
       },
