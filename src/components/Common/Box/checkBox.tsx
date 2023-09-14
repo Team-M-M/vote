b32ac9ae0b00eec956a41a440b3ba611482aa326
Parkwochang <whwh3560@gmail.com>
2023-09-14 12:01:11 +0900

    refactor: [front] agreebox -> checkbox로 변경

--- src/components/Common/Box/checkBox.tsx
+++ src/components/Common/Box/checkBox.tsx
@@ -1,17 +1,35 @@
 'use client';
+import { useId } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 // interface TextFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
 //   error?: boolean;
 // }
 
-export const CheckBox = ({ type, name }: any) => {
+export type CheckBoxType = 'checkbox' | 'radio';
+interface CheckBoxProps {
+  type?: CheckBoxType;
+  name: string;
+  value?: string;
+}
+
+export const CheckBox = ({ type = 'radio', name, value = 'true', ...props }: CheckBoxProps) => {
   const { register } = useFormContext();
+  const generatedId = useId();
 
   return (
     <>
-      <label className="check_box" htmlFor={name}>
-        <input {...register('checked')} value={`${name}`} type={type} id={name} />
+      <label className="check_box mx-3" htmlFor={generatedId}>
+        <input
+          {...register(name, {
+            required: '찬성 반대를 선택해주세요.',
+          })}
+          value={value}
+          type={type}
+          id={generatedId}
+          name={name}
+          {...props}
+        />
         <span className="icon"></span>
       </label>
     </>
