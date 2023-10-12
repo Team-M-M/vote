73ed7aa0037344506e3f411fc4c115f645c0c1c4
Chang Woo Park <whwh3560@gmail.com>
2023-10-12 13:21:13 +0900

    fix: [front] cookie

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,17 @@
 import { BackHeader } from '@components/Common/Header/backHead';
 import AccountPage from '@components/Container/AccountPage';
+import { cookies } from 'next/headers';
 
 const Page = () => {
+  const cookie = cookies();
+  const { name, id, detail, phone } = JSON.parse(cookie.get('user')?.value!);
+
   return (
     <main>
       <BackHeader />
       <AccountPage />
+      <p>{phone}</p>
+      <p>{name}</p>
       {/* <KeyPad /> */}
     </main>
   );
